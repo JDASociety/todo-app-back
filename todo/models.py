30e8a1b1d9c7f9855862e4443002dbb242d1a2d8
diegosiac <diego35502@hotmail.com>
2024-02-24 01:14:51 -0600

    Update TodoModel in models.py

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 
 
 class TodoModel(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=250)
-    description = models.TextField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=70)
+    description = models.TextField(max_length=250)
     done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
