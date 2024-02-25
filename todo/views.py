1d0ceee9774bca3285fa7d9770f537cb324c3a60
diegosiac <diego35502@hotmail.com>
2024-02-24 23:07:33 -0600

    Add latest_todo action to TodoModelViewSet

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet, ViewSet
+from rest_framework.response import Response
 from .models import TodoModel
 from .serializers import TodoSerializer
+
+from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
 
-class TodoModelViewSet(viewsets.ModelViewSet):
+class TodoModelViewSet(ModelViewSet):
     queryset = TodoModel.objects.all()
     serializer_class = TodoSerializer
+
+    @action(detail=False, methods=['get'], url_path='latest-todo')
+    def latest_todo(self, request):
+        try:
+            latest_todo = TodoModel.objects.latest('updated_at')
+            serializer = self.get_serializer(latest_todo)
+            return Response(serializer.data)
+        except TodoModel.DoesNotExist:
+            return Response({"message": "No hay tareas disponibles."}, status=status.HTTP_404_NOT_FOUND)
