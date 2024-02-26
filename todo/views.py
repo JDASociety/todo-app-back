853e304e00920d08960edb95b61941a47c77d75a
diegosiac <diego35502@hotmail.com>
2024-02-26 00:14:54 -0600

    Add user field to TodoSerializer and implement authentication and permission checks in TodoViewSet

--- todo/views.py
+++ todo/views.py
@@ -1,23 +1,36 @@
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
 from .models import Todo
 from .serializers import TodoSerializer
-
+from user.authentication import UserJWTAuthentication
 
 # Create your views here.
+
 
 class TodoViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [UserJWTAuthentication]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        print(self.request.user)
+        serializer.save(user=self.request.user)
 
     @action(detail=False, methods=['get'], url_path='latest-todo')
     def latest_todo(self, request):
         try:
-            latest_todo = Todo.objects.latest('updated_at')
+            latest_todo = self.get_queryset().latest('updated_at')
             serializer = self.get_serializer(latest_todo)
+
             return Response(serializer.data)
         except Todo.DoesNotExist:
             return Response({"message": "No hay tareas disponibles."}, status=status.HTTP_404_NOT_FOUND)
@@ -25,8 +38,10 @@ class TodoViewSet(ModelViewSet):
     @action(detail=False, methods=['get'], url_path='status-todo')
     def get_status_todo(self, request):
         try:
-            total_todos = Todo.objects.count()
-            completed_todos = Todo.objects.filter(done=True).count()
+            user_todos = self.get_queryset()
+
+            total_todos = user_todos.count()
+            completed_todos = user_todos.filter(done=True).count()
             incomplete_todos = total_todos - completed_todos
 
             data = {
