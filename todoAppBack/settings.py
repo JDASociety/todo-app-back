5004e78a170c1e497d06efb6d2a73d714e3018d4
diegosiac <diego35502@hotmail.com>
2024-02-26 00:14:58 -0600

    Add token expiration time configuration

--- todoAppBack/settings.py
+++ todoAppBack/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import environ
-import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +119,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+}
+
+SIMPLE_JWT = {
+    # Token de acceso válido por 1 día
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    # Token de actualización válido por 1 día
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 # Internationalization
